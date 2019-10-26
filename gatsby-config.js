93d552ec5c53621201696f188a4c71e331154899
Michal Opalka <michalopalka998@gmail.com>
2019-10-26 21:54:31 +0200

    Created starter files

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    title: `Estates`,
+    description: `This is simple site to show estates.`,
+    author: `@offblack`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Estates`,
+        short_name: `Estates`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `standalone`,
+        icon: `src/images/estates-icon.png`,
+      },
+    },
+  ],
+};
